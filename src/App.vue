30904e17284c67e7214440b075fca757b4e046e7
Lukáš Viliams <lukas.viliams@gmail.com>
2021-12-25 21:48:12 +0100

    es lint setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
+/* eslint-disable vue/no-multiple-template-root */
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">
+      Home
+    </router-link> |
+    <router-link to="/about">
+      About
+    </router-link>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
