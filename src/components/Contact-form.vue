7880fa03c9cd9cfd90e3c64c795d8bb1cf4fec07
Lukáš Viliams <lukas.viliams@gmail.com>
2022-01-21 21:00:29 +0100

    contact form design desktop

--- src/components/Contact-form.vue
+++ src/components/Contact-form.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="contact_form_parrent">
-    <p>I am form</p>
+    <div class="content_container">
+      <div class="content_container-header">
+        <p class="font-h2">
+          Contact me
+        </p>
+      </div>
+      <div class="content_container-content">
+        <form class="content_container-form">
+          <label
+            for="text"
+          >Name</label>
+          <input
+            id="text"
+            type="text"
+            placeholder="Jane Apleseed"
+            name="text"
+            class="input_text"
+          >
+          <label
+            for="email"
+          >Email address</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="email@example.com"
+            class="input_email"
+          >
+          <label
+            for="textarea"
+          >Message</label>
+          <textarea
+            id="textarea"
+            name="textarea"
+            placeholder="How can I help"
+            class="input_textarea"
+          />
+          <button class="btn-primary">
+            Send message
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +52,72 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.content_container {
+  margin: 0 8vw 6vh 8vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.content_container-header{
+  width: 49%;
+  display: flex;
+  justify-content: flex-start;
+}
+.content_container-content {
+  width: 51%;
+  display: flex;
+  justify-content: flex-start;
+}
+.content_container-form {
+  display: flex;
+  flex-direction: column;
+}
+form {
+  width: 100%;
+}
+input {
+  height: 4.8rem;
+}
+textarea {
+  height: 9.6rem;
+  resize: none;
+}
+input, textarea {
+  position: relative;
+  background-color: var(--light-gray);
+  border: none;
+  margin-bottom: 2.5rem;
+  transition: all .3s ease-in-out;
+}
+input:focus, textarea:focus {
+  outline: none;
+}
+input::placeholder, textarea::placeholder {
+position: absolute;
+left: 3%;
+top: 15%;
 
+font-size: 13px;
+font-family: 'Public Sans', sans-serif;
+line-height: 30px;
+letter-spacing: 0;
+}
+input:focus::placeholder, textarea:focus::placeholder {
+transition: all .3s ease-in-out;
+color: transparent;
+}
+label {
+  font-size: 13px;
+  font-family: 'Public Sans', sans-serif;
+  font-weight: bold;
+  line-height: 30px;
+  letter-spacing: 0;
+  text-align: left;
+  margin-bottom: 1rem;
+}
+.btn-primary{
+  width: 15.2rem;
+  justify-content: center;
+}
 </style>
